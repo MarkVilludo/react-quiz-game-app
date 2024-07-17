8c8cba5e73ca953db2f4daaf2cbf1b1f04259745
Arnel W. Bridges <arnel.wbrdiges@gmail.com>
2024-07-17 15:14:33 +0800

    style: adjust mobile responsive

--- src/components/Start.js
+++ src/components/Start.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React from 'react';
 
-export default function Start(props)
-{
-    return(<div className='start-container'>
-        <h1 className='start-container-title'>Quizzical</h1>
-        <h2 className='start-container-subtitle'>Query Your Brain</h2>
-        <button className='start-container-button' onClick={props.startQuiz}>Start Quiz</button>
-    </div>)
+export default function Start(props) {
+  return (
+    <div className="start-container">
+      <h1 className="start-container-title">Quizzical</h1>
+      <h2 className="start-container-subtitle">Query Your Brain</h2>
+      <button className="start-container-button" onClick={props.startQuiz}>
+        Start Quiz
+      </button>
+    </div>
+  );
 }
